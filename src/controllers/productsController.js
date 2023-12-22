2f797895ad2e48c245e2e16db24bf13925e5f46d
joseluismolina75 <joseluismolina75@gmail.com>
2023-12-22 14:45:04 -0300

    métodos store y edi

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const  {v4 : uuidv4} = require('uuid'); 
 
 const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
 const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
@@ -9,27 +10,45 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
+		res.render('products.ejs', {products})
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		// Do the magic
+		const pSelected = products.find(product => product.id == req.params.id);
+		console.log(req.params.id);
+		res.render('detail.ejs', {pSelected});
 	},
 
 	// Create - Form to create
 	create: (req, res) => {
-		// Do the magic
+		res.render("product-create-form.ejs");
 	},
 	
 	// Create -  Method to store
 	store: (req, res) => {
-		// Do the magic
+		//creamos nuevo producto del formulario con req.body
+		const newProduct = {
+			//id: products.length + 1,
+			id: uuidv4 (),				//id único uuid
+			image: 'default-image.png',	//imagen por defecto
+			...req.body					//usamos propagación o spread operator
+
+		}
+		console.log(newProduct);
+		//agrego nuevo producto al listado
+		products.push(newProduct);	//push()añade uno o más elementos al final de un array y devuelve la nueva longitud del array.
+		//Convertir a JSON y escribir en la BD JSON
+		fs.writeFileSync(productsFilePath, JSON.stringify(products))
+		//redireccionamos al listado de productos
+		res.redirect('/products');
 	},
 
 	// Update - Form to edit
 	edit: (req, res) => {
-		// Do the magic
+		console.log(req.params.id)
+		const pToEdit = products.find(product => product.id = req.params.id)
+		res.render('product-edit-form.ejs', {pToEdit})
 	},
 	// Update - Method to update
 	update: (req, res) => {
