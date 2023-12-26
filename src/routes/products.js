8e3bc48302fbba05975c196dcd77b3f0b615df44
joseluismolina75 <joseluismolina75@gmail.com>
2023-12-26 16:03:18 -0300

    ultima modificacion

--- src/routes/products.js
+++ src/routes/products.js
@@ -6,19 +6,18 @@ const router = express.Router();
 const productsController = require('../controllers/productsController');
 
 /*** GET ALL PRODUCTS http://localhost:3000/products ***/ 
-router.get('/', productsController.index); 
+router.get('/', productsController.index);                          //raiz
 
 /*** GET ONE PRODUCT http://localhost:3000/products/detail/1 ***/ 
 router.get('/detail/:id', productsController.detail); 
 
 /*** CREATE ONE PRODUCT http://localhost:3000/products/create ***/ 
-router.get('/create/', productsController.create); 
-router.post('/create', productsController.store); 
+router.get('/create/', productsController.create);      //usamos get para mostrar el formulario de creación ejs
+router.post('/create', productsController.store);       //usamos post para guardar la información
 
 /*** EDIT ONE PRODUCT http://localhost:3000/products/edit/1 ***/ 
 router.get('/edit/:id', productsController.edit); 
 router.put('/edit/:id', productsController.update); 
-
 
 /*** DELETE ONE PRODUCT***/ 
 router.delete('/delete/:id', productsController.destroy); 
